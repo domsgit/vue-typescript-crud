733c343bc87c9fced52fdc423ce16b355708b29f
domsgit <domsgit@outlook.com>
2019-08-22 18:47:16 +0800

    add component hooks to vue component

---
+++ src/utils/class-component-hooks.ts
@@ -0,0 +1,12 @@
+/**
+ * https://github.com/vuejs/vue-class-component
+ */
+// class-component-hooks.js
+import Component from "vue-class-component";
+
+// Register the router hooks with their names
+Component.registerHooks([
+  "beforeRouteEnter",
+  "beforeRouteLeave",
+  "beforeRouteUpdate" // for vue-router 2.2+
+]);
